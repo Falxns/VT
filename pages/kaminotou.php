63df5c43d83590056e45994389767fe1b5e9dd7c
Maksim <maks.falonse@gmail.com>
2020-05-13 13:36:40 +0300

    added materials from lab1

---
+++ pages/kaminotou.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<meta charset="utf-8"/>
+<html>
+    <head>
+        <title>Башня бога</title>
+        <link rel="stylesheet" type="text/css" href="../styles/description.css">
+        <link rel="stylesheet" type="text/css" href="../styles/nav.css">
+        <link rel="stylesheet" type="text/css" href="../styles/footer.css">
+    </head>
+    <body>
+        <header>
+            <ul class="navigation">
+                <li>
+                    <a class="navtab" href="../index.php">ГЛАВНАЯ</a>
+                </li>
+                <li>
+                    <a class="navtab" href="./team.php">КОМАНДА</a>
+                </li>
+                <li>
+                    <a class="navtab" href="./support.php">ПОДДЕРЖКА</a>
+                </li>
+                <li>
+                    <a class="navtab" href="./labs.php">ЛАБОРАТОРНЫЕ</a>
+                </li>
+            </ul>
+        </header>
+        <main>
+            <h2 class="block-main-header">Башня бога:</h2>
+            <div class="anime-container">
+                <iframe class="anime-container_video" src="https://www.youtube.com/embed/1nzYY-dYqn8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <ul class="anime-container_list">
+                    <li>Рейтинг: </li>
+                    <li>Количество просмотров: </li>
+                    <li>Дата выхода: </li>
+                    <li>Жанр: </li>
+                </ul>
+                <h3 class="anime-container_description-header">Описание:</h3>
+                <p class="anime-container_description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione ea optio minima perferendis rem. Commodi dignissimos iure magni facilis delectus! Tempore assumenda nihil ex sint quidem minima inventore. Tempora, vero. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, quam consectetur obcaecati in deserunt fugiat quaerat. Architecto id distinctio totam recusandae praesentium, aliquid possimus minus obcaecati, vero, eligendi impedit esse.</p>
+            </div>
+        </main>
+        <footer class="footer-container">
+            <a class="contacts" href="https://vk.com/maxim_falonse" target="_blank">
+                <img class="icon" src="../vk-logo.png">
+            </a>
+            <a class="contacts" href="https://www.instagram.com/falxns_/" target="_blank">
+                <img class="icon" src="../inst-logo.png">
+            </a>
+            <p class="email">maks.falonse@gmail.com</p>
+            <p class="phone">+375295656229</p>
+        </footer>
+    </body>
+</html>
---
+++ pages/labs.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<meta charset="utf-8"/>
+<html>
+    <head>
+        <title>Лабораторные</title>
+        <link rel="stylesheet" type="text/css" href="../styles/team.css">
+        <link rel="stylesheet" type="text/css" href="../styles/nav.css">
+        <link rel="stylesheet" type="text/css" href="../styles/footer.css">
+    </head>
+    <body>
+        <header>
+            <ul class="navigation">
+                <li>
+                    <a class="navtab" href="../index.php">ГЛАВНАЯ</a>
+                </li>
+                <li>
+                    <a class="navtab" href="./team.php">КОМАНДА</a>
+                </li>
+                <li>
+                    <a class="navtab" href="./support.php">ПОДДЕРЖКА</a>
+                </li>
+                <li>
+                    <a class="navtab" href="./labs.php">ЛАБОРАТОРНЫЕ</a>
+                </li>
+            </ul>
+        </header>
+        <main>
+            <h2 class="block-main-header">Пока тут пусто :с</h2>
+        </main>
+        <footer class="footer-container">
+            <a class="contacts" href="https://vk.com/maxim_falonse" target="_blank">
+                <img class="icon" src="../vk-logo.png">
+            </a>
+            <a class="contacts" href="https://www.instagram.com/falxns_/" target="_blank">
+                <img class="icon" src="../inst-logo.png">
+            </a>
+            <p class="email">maks.falonse@gmail.com</p>
+            <p class="phone">+375295656229</p>
+        </footer>
+    </body>
+</html>
