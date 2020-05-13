3db92534ad041ab8037c4711fd350622d2cd03bf
Maksim <maks.falonse@gmail.com>
2020-05-13 18:09:20 +0300

    added calculation of sum of digits in number

--- pages/labs.php
+++ pages/labs.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <title>Лабораторные</title>
-        <link rel="stylesheet" type="text/css" href="../styles/team.css">
+        <link rel="stylesheet" type="text/css" href="../styles/support.css">
         <link rel="stylesheet" type="text/css" href="../styles/nav.css">
         <link rel="stylesheet" type="text/css" href="../styles/footer.css">
     </head>
@@ -12,7 +12,22 @@
             include '../styles/navigation.php';
         ?>
         <main>
-            <h2 class="block-main-header">Пока тут пусто :с</h2>
+            <form action="<?php $_PHP_SELF ?>" method="GET" id="lab-2" class="user-form">
+                <h2 class="block-main-header">Лабораторная работа 2, вариант 9</h2>
+                <label class="user-form_label" for="number-input">Введите число: </label>
+                <input class="user-form_input" name="number-input" pattern="^[0-9]+$">
+                <input class="user-form_submit" type="submit" value="Send">
+                <?php
+                    if( isset($_GET["number-input"])) {
+                        $number = $_GET["number-input"];
+                        $answer = 0;
+                        for ($i = 0; $i < strlen($number); $i++) {
+                            $answer += $number[$i];
+                        }
+                        echo "<label class='user-form_label'>Ответ: $answer</label>";
+                    }
+                ?>
+            </form>
         </main>
         <footer class="footer-container">
             <a class="contacts" href="https://vk.com/maxim_falonse" target="_blank">
