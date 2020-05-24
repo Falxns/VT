a6a55d64e0d36f7054f159794e0679ef4dcc9c4d
Maksim <maks.falonse@gmail.com>
2020-05-25 01:08:48 +0300

    translated anime pages

--- pages/evangelion.php
+++ pages/evangelion.php
@@ -8,41 +8,28 @@
 <meta charset="utf-8"/>
 <html>
     <head>
-        <title>Евангелион</title>
+        <title><?php echo $textLanguage["MAIN_TITLES_EVA"];?></title>
         <link rel="stylesheet" type="text/css" href="../styles/description.css">
         <link rel="stylesheet" type="text/css" href="../styles/nav.css">
         <link rel="stylesheet" type="text/css" href="../styles/footer.css">
         <link rel="stylesheet" type="text/css" href="../styles/support.css">
     </head>
     <body>
-        <header>
-            <ul class="navigation">
-                <li class="navigation_item">
-                    <a class="navtab" href="../index.php">ГЛАВНАЯ</a>
-                </li>
-                <li class="navigation_item">
-                    <a class="navtab" href="./team.php">КОМАНДА</a>
-                </li>
-                <li class="navigation_item">
-                    <a class="navtab" href="./support.php">ПОДДЕРЖКА</a>
-                </li>
-                <li class="navigation_item">
-                    <a class="navtab" href="./labs.php">ЛАБОРАТОРНЫЕ</a>
-                </li>
-            </ul>
-        </header>
+        <?php 
+            include '../styles/navigation.php';
+        ?>
         <main>
-            <h2 class="block-main-header">Евангелион:</h2>
+            <h2 class="block-main-header"><?php echo $textLanguage["MAIN_TITLES_EVA"];?>:</h2>
             <div class="anime-container">
                 <iframe class="anime-container_video" src="https://www.youtube.com/embed/t-QSmNReDyI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <ul class="anime-container_list">
-                    <li>Рейтинг: </li>
-                    <li>Количество просмотров: </li>
-                    <li>Дата выхода: </li>
-                    <li>Жанр: </li>
+                    <li><?php echo $textLanguage["ANIME_RANK"];?></li>
+                    <li><?php echo $textLanguage["ANIME_VIEWS"];?></li>
+                    <li><?php echo $textLanguage["ANIME_RELEASEDATE"];?></li>
+                    <li><?php echo $textLanguage["ANIME_GENRE"];?></li>
                 </ul>
-                <h3 class="anime-container_description-header">Описание:</h3>
-                <p class="anime-container_description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione ea optio minima perferendis rem. Commodi dignissimos iure magni facilis delectus! Tempore assumenda nihil ex sint quidem minima inventore. Tempora, vero. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, quam consectetur obcaecati in deserunt fugiat quaerat. Architecto id distinctio totam recusandae praesentium, aliquid possimus minus obcaecati, vero, eligendi impedit esse.</p>
+                <h3 class="anime-container_description-header"><?php echo $textLanguage["ANIME_DESCRIPTION_HEADER"];?></h3>
+                <p class="anime-container_description"><?php echo $textLanguage["ANIME_DESCRIPTION"];?></p>
             </div>
         </main>
         <?php 
